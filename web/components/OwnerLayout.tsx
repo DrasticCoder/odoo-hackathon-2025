0a19f7bb9b30195a9528c76dbeaec36952ad6a9c
Deep Bansode <drasticoder@gmail.com>
2025-08-12 01:14:59 +0530

    fix: standardize string quotes in OwnerLayout component

--- web/components/OwnerLayout.tsx
+++ web/components/OwnerLayout.tsx
@@ -8,11 +8,9 @@ interface OwnerLayoutProps {
 
 export function OwnerLayout({ children }: OwnerLayoutProps) {
   return (
-    <div className="flex h-screen bg-background">
+    <div className='bg-background flex h-screen'>
       <OwnerSidebar />
-      <main className="flex-1 overflow-auto">
-        {children}
-      </main>
+      <main className='flex-1 overflow-auto'>{children}</main>
     </div>
   );
 }
